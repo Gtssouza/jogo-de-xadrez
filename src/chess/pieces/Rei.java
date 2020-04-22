52cf865f8e5160770911c05d5309b63aa4c47b16
Gustavo Souza <gtssouza94@gmail.com>
2020-04-22 11:12:46 -0300

    Implementando movimentos possiveis do Rei

--- src/chess/pieces/Rei.java
+++ src/chess/pieces/Rei.java
@@ -1,26 +1,82 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Rei extends ChessPiece{
+public class Rei extends ChessPiece {
 
 	public Rei(Board board, Color color) {
 		super(board, color);
-		
+
 	}
-	
+
 	@Override
 	public String toString() {
 		return "R";
 	}
 
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
+	}
+
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position p = new Position(0, 0);
+
+		// acima
+		p.setValues(position.getRow() - 1, position.getColumn());
+		if (getBoard().positionExistis(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// abaixo
+		p.setValues(position.getRow() + 1, position.getColumn());
+		if (getBoard().positionExistis(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// esquerda
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		if (getBoard().positionExistis(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// direita
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		if (getBoard().positionExistis(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// diagonal superior esquerda
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if (getBoard().positionExistis(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// diagonal superior direita
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if (getBoard().positionExistis(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// diagonal inferior esquerda
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if (getBoard().positionExistis(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// diagonal inferior direita
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if (getBoard().positionExistis(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
 		return mat;
 	}
-	
 
 }
